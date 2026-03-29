6f283943c35e5eefbfd18a00157ca5c97688aca7
Andrew Garcia <agarc108@charlotte.edu>
2026-03-29 17:09:10 -0400

    PBI: React Router Setup 2/2

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -1,48 +1,37 @@
 import React from 'react';
 import {
-  Divider,
-  List,
-  ListItem,
-  ListItemText,
-  Typography,
-}
-from '@mui/material';
+  Divider, List, ListItem, ListItemText
+} from '@mui/material';
+import { Link } from 'react-router-dom';
 import './userList.css';
 
 /**
- * Define UserList, a React component of project #5
+ * Define UserList, a React component of project #5.
  */
 class UserList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
+    const users = window.models.userListModel();
+
     return (
-      <div>
-        <Typography variant="body1">
-          This is the user list, which takes up 3/12 of the window.
-          You might choose to use <a href="https://mui.com/components/lists/">Lists</a> and <a href="https://mui.com/components/dividers/">Dividers</a> to
-          display your users like so:
-        </Typography>
-        <List component="nav">
-          <ListItem>
-            <ListItemText primary="Item #1" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #2" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #3" />
-          </ListItem>
-          <Divider />
-        </List>
-        <Typography variant="body1">
-          The model comes in from window.models.userListModel()
-        </Typography>
-      </div>
+      <List component="nav">
+        {users.map((user, index) => (
+          <div key={user._id}>
+            <ListItem
+              button
+              component={Link}
+              to={`/users/${user._id}`}
+            >
+              <ListItemText primary={`${user.first_name} ${user.last_name}`} />
+            </ListItem>
+            {index < users.length - 1 && <Divider />}
+          </div>
+        ))}
+      </List>
+    );
+  }
+}
+
+export default UserList;
     );
   }
 }
