fad81fb937a4ae9b638ea6662908e4c390777ef0
Nathaniel Rivera <nriver10@charlotte.edu>
2026-03-29 17:54:04 -0400

    Update userDetail.jsx

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,27 +1,56 @@
 import React from 'react';
-import {
-  Typography
-} from '@mui/material';
+import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './userDetail.css';
-
 
 /**
  * Define UserDetail, a React component of project #5
  */
 class UserDetail extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
+    const userId = this.props.match.params.userId;
+    const user = window.models.userModel(userId);
+
+    if (!user) {
+      return (
+        <Typography variant="body1">
+          User not found.
+        </Typography>
+      );
+    }
+
     return (
-      <Typography variant="body1">
-        This should be the UserDetail view of the PhotoShare app. Since
-        it is invoked from React Router the params from the route will be
-        in property match. So this should show details of user:
-        {this.props.match.params.userId}. You can fetch the model for the
-        user from window.models.userModel(userId).
-      </Typography>
+      <div className="user-detail">
+        <Typography variant="h4" gutterBottom>
+          {user.first_name} {user.last_name}
+        </Typography>
+
+        <Typography variant="body1">
+          <strong>First Name:</strong> {user.first_name}
+        </Typography>
+
+        <Typography variant="body1">
+          <strong>Last Name:</strong> {user.last_name}
+        </Typography>
+
+        <Typography variant="body1">
+          <strong>Location:</strong> {user.location}
+        </Typography>
+
+        <Typography variant="body1">
+          <strong>Occupation:</strong> {user.occupation}
+        </Typography>
+
+        <Typography variant="body1">
+          <strong>Description:</strong> {user.description}
+        </Typography>
+
+        <div className="photo-link-container">
+          <Link to={`/photos/${user._id}`} className="photo-link">
+            View {user.first_name}'s Photos
+          </Link>
+        </div>
+      </div>
     );
   }
 }
