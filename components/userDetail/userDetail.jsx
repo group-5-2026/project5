d19057d1fcb94d60fe11db6045a2b714fe575700
Nathaniel Rivera <nriver10@charlotte.edu>
2026-03-29 21:53:23 -0400

    Update userDetail.jsx

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-    Box,
-    Button,
-    TextField
-} from '@mui/material';
+import { Box, Button, TextField } from '@mui/material';
 import './userDetail.css';
 import fetchModel from "../../lib/fetchModelData";
 
@@ -17,70 +13,105 @@ class UserDetail extends React.Component {
             user: undefined
         };
     }
+
     componentDidMount() {
-        const new_user_id = this.props.match.params.userId;
-        this.handleUserChange(new_user_id);
+        const newUserId = this.props.match.params.userId;
+        this.handleUserChange(newUserId);
     }
 
-    componentDidUpdate() {
-        const new_user_id = this.props.match.params.userId;
-        const current_user_id = this.state.user?._id;
-        if (current_user_id  !== new_user_id){
-            this.handleUserChange(new_user_id);
+    componentDidUpdate(prevProps) {
+        const newUserId = this.props.match.params.userId;
+        const currentUserId = this.state.user?._id;
+
+        if (currentUserId !== newUserId) {
+            this.handleUserChange(newUserId);
         }
     }
 
-    handleUserChange(user_id){
-        fetchModel("/user/" + user_id)
-            .then((response) =>
-            {
-                const new_user = response.data;
-                this.setState({
-                    user: new_user
-                });
-                const main_content = "User Details for " + new_user.first_name + " " + new_user.last_name;
-                this.props.changeMainContent(main_content);
+    handleUserChange(userId) {
+        fetchModel(`/user/${userId}`)
+            .then((response) => {
+                const newUser = response.data;
+                this.setState({ user: newUser });
+
+                // Escape apostrophe in JSX string just in case
+                const mainContent = `User Details for ${newUser.first_name} ${newUser.last_name}`;
+                this.props.changeMainContent(mainContent);
+            })
+            .catch((err) => {
+                console.error("Error fetching user:", err);
             });
     }
 
     render() {
-        return this.state.user ? (
-            <div>
-                <Box component="form" noValidate autoComplete="off">
-                    <div>
-                        <Button variant="contained" component="a" href={"#/photos/" + this.state.user._id}>
-                            User Photos
-                        </Button>
-                    </div>
-                    <div>
-                        <TextField id="first_name" label="First Name" variant="outlined" disabled fullWidth
-                                   margin="normal"
-                                   value={this.state.user.first_name}/>
-                    </div>
-                    <div>
-                        <TextField id="last_name" label="Last Name" variant="outlined" disabled fullWidth
-                                   margin="normal"
-                                   value={this.state.user.last_name}/>
-                    </div>
-                    <div>
-                        <TextField id="location" label="Location" variant="outlined" disabled fullWidth
-                                   margin="normal"
-                                   value={this.state.user.location}/>
-                    </div>
-                    <div>
-                        <TextField id="description" label="Description" variant="outlined" multiline rows={4}
-                                   disabled
-                                   fullWidth margin="normal" value={this.state.user.description}/>
-                    </div>
-                    <div>
-                        <TextField id="occupation" label="Occupation" variant="outlined" disabled fullWidth
-                                   margin="normal"
-                                   value={this.state.user.occupation}/>
-                    </div>
+        const { user } = this.state;
+
+        if (!user) return <div />;
+
+        return (
+            <Box component="form" noValidate autoComplete="off">
+                <Box mb={2}>
+                    <Button
+                        variant="contained"
+                        component="a"
+                        href={`#/photos/${user._id}`}
+                    >
+                        User Photos
+                    </Button>
                 </Box>
-            </div>
-        ) : (
-            <div/>
+
+                <TextField
+                    id="first_name"
+                    label="First Name"
+                    variant="outlined"
+                    disabled
+                    fullWidth
+                    margin="normal"
+                    value={user.first_name}
+                />
+
+                <TextField
+                    id="last_name"
+                    label="Last Name"
+                    variant="outlined"
+                    disabled
+                    fullWidth
+                    margin="normal"
+                    value={user.last_name}
+                />
+
+                <TextField
+                    id="location"
+                    label="Location"
+                    variant="outlined"
+                    disabled
+                    fullWidth
+                    margin="normal"
+                    value={user.location}
+                />
+
+                <TextField
+                    id="description"
+                    label="Description"
+                    variant="outlined"
+                    multiline
+                    rows={4}
+                    disabled
+                    fullWidth
+                    margin="normal"
+                    value={user.description}
+                />
+
+                <TextField
+                    id="occupation"
+                    label="Occupation"
+                    variant="outlined"
+                    disabled
+                    fullWidth
+                    margin="normal"
+                    value={user.occupation}
+                />
+            </Box>
         );
     }
 }
