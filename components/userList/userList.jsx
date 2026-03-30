14a09640d9fb1599e2bfc46744d961c70128ca62
Jennifer Vazquez <jvazqu20@charlotte.edu>
2026-03-29 21:56:46 -0400

    Refactor UserList component structure and styling
    Refactor UserList component to include a header and use ListItemButton for better accessibility.

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -1,37 +1,52 @@
 import React from 'react';
 import {
-  Divider, List, ListItem, ListItemText
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+  Typography,
 } from '@mui/material';
 import { Link } from 'react-router-dom';
 import './userList.css';
 
 /**
- * Define UserList, a React component of project #5.
+ * Define UserList, a React component of project #5
  */
 class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    // Fetch users from the model
     const users = window.models.userListModel();
 
     return (
-      <List component="nav">
-        {users.map((user, index) => (
-          <div key={user._id}>
-            <ListItem
-              button
-              component={Link}
-              to={`/users/${user._id}`}
-            >
-              <ListItemText primary={`${user.first_name} ${user.last_name}`} />
-            </ListItem>
-            {index < users.length - 1 && <Divider />}
-          </div>
-        ))}
-      </List>
-    );
-  }
-}
+      <div>
+        {/* Optional header */}
+        <Typography variant="h6" sx={{ padding: '10px' }}>
+          Users
+        </Typography>
 
-export default UserList;
+        <List component="nav">
+          {users.map((user) => (
+            <React.Fragment key={user._id}>
+              <ListItem disablePadding>
+                <ListItemButton
+                  component={Link}
+                  to={`/users/${user._id}`}
+                >
+                  <ListItemText
+                    primary={`${user.first_name} ${user.last_name}`}
+                  />
+                </ListItemButton>
+              </ListItem>
+              <Divider />
+            </React.Fragment>
+          ))}
+        </List>
+      </div>
     );
   }
 }
