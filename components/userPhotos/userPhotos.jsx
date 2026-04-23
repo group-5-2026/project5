daf283dfab4effd1d5d2aaa2d8397b99a17a491a
Nathaniel Rivera <nriver10@charlotte.edu>
2026-04-22 20:59:03 -0400

    implemented photo comments feature

--- components/userPhotos/userPhotos.jsx
+++ components/userPhotos/userPhotos.jsx
@@ -3,19 +3,17 @@ import {
   Button, TextField,
   ImageList, ImageListItem, Typography
 } from '@mui/material';
-import axios from 'axios'; // Replace fetchModel import with axios
+import axios from 'axios';
 import './userPhotos.css';
 
-/**
- * Define UserPhotos, a React component of project #5
- */
 class UserPhotos extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       user_id: undefined,
       photos: [],
-      error: null
+      error: null,
+      commentTexts: {}   // per-photo comment storage
     };
   }
 
@@ -33,89 +31,200 @@ class UserPhotos extends React.Component {
   }
 
   handleUserChange(user_id) {
-    // 1. Fetch User Info (to update the TopBar/MainContent title)
     axios.get(`http://localhost:3001/user/${user_id}`)
       .then((userResponse) => {
         const user = userResponse.data;
         const main_content = `User Photos for ${user.first_name} ${user.last_name}`;
-        
+
         if (typeof this.props.changeMainContent === "function") {
           this.props.changeMainContent(main_content);
         }
       })
-      .catch((err) => console.error("Error fetching user for header:", err));
+      .catch((err) => console.error("Error fetching user:", err));
 
-    // 2. Fetch Photos using the new endpoint
     axios.get(`http://localhost:3001/photosOfUser/${user_id}`)
       .then((photoResponse) => {
         this.setState({
-          user_id: user_id,
+          user_id,
           photos: photoResponse.data,
           error: null
         });
       })
       .catch((err) => {
         console.error("Error fetching photos:", err);
-        this.setState({ 
-          user_id: user_id, 
-          photos: [], 
-          error: "No photos found for this user" 
+        this.setState({
+          user_id,
+          photos: [],
+          error: "No photos found for this user"
         });
       });
   }
 
+  //  handle text input per photo
+  handleCommentChange = (photoId, value) => {
+    this.setState((prevState) => ({
+      commentTexts: {
+        ...prevState.commentTexts,
+        [photoId]: value
+      }
+    }));
+  };
+
+  //  submit comment
+  handleSubmitComment = (photoId) => {
+    const comment = this.state.commentTexts[photoId];
+
+    if (!comment || comment.trim() === "") return;
+
+    axios.post(
+      `http://localhost:3001/commentsOfPhoto/${photoId}`,
+      { comment }
+    )
+    .then(() => {
+      return axios.get(
+        `http://localhost:3001/photosOfUser/${this.state.user_id}`
+      );
+    })
+    .then((response) => {
+      this.setState((prevState) => ({
+        photos: response.data,
+        commentTexts: {
+          ...prevState.commentTexts,
+          [photoId]: ""
+        }
+      }));
+    })
+    .catch((err) => {
+      console.error("Error posting comment:", err);
+    });
+  };
+
   render() {
     if (this.state.error) {
-      return <Typography sx={{ m: 2 }} color="error">{this.state.error}</Typography>;
+      return (
+        <Typography sx={{ m: 2 }} color="error">
+          {this.state.error}
+        </Typography>
+      );
     }
 
-// Inside your render return...
-return this.state.user_id ? (
-  <div className="user-photos-view"> {/* Added Class */}
-    <div>
-      <Button variant="contained" component="a" href={"#/users/" + this.state.user_id}>
-        User Detail
-      </Button>
-    </div>
-    
-    <ImageList variant="masonry" cols={1} gap={24}>
-      {this.state.photos.map((item) => (
-        <div key={item._id} className="photo-card"> {/* Added Class */}
-          <TextField 
-            label="Photo Date" variant="outlined" disabled fullWidth margin="normal"
-            value={item.date_time} 
-          />
-          
-          <ImageListItem key={item.file_name}>
-            <img
-              src={`http://localhost:3001/images/${item.file_name}`}
-              alt={item.file_name}
-              loading="lazy"
-            />
-          </ImageListItem>
+    return this.state.user_id ? (
+      <div className="user-photos-view">
 
-          {item.comments ? (
-            item.comments.map((comment) => (
-              <div key={comment._id} className="comment-block"> {/* Added Class */}
-                <TextField label="Comment Date" variant="outlined" disabled fullWidth
-                           margin="normal" value={comment.date_time} size="small" />
-                <TextField label="User" variant="outlined" disabled fullWidth
-                           margin="normal" value={comment.user.first_name + " " + comment.user.last_name}
-                           component="a" href={"#/users/" + comment.user._id} size="small" />
-                <TextField label="Comment" variant="outlined" disabled fullWidth
-                           margin="normal" multiline rows={2} value={comment.comment} size="small" />
-              </div>
-            ))
-          ) : (
-            <TextField label="No Comments" variant="outlined" disabled fullWidth margin="normal" />
-          )}
+        <div>
+          <Button
+            variant="contained"
+            component="a"
+            href={"#/users/" + this.state.user_id}
+          >
+            User Detail
+          </Button>
         </div>
-      ))}
-    </ImageList>
-  </div>
-) : (
-  <div />
-);
+
+        <ImageList variant="masonry" cols={1} gap={24}>
+          {this.state.photos.map((item) => (
+            <div key={item._id} className="photo-card">
+
+              <TextField
+                label="Photo Date"
+                variant="outlined"
+                disabled
+                fullWidth
+                margin="normal"
+                value={item.date_time}
+              />
+
+              <ImageListItem>
+                <img
+                  src={`http://localhost:3001/images/${item.file_name}`}
+                  alt={item.file_name}
+                  loading="lazy"
+                />
+              </ImageListItem>
+
+              {/* COMMENTS */}
+              {item.comments && item.comments.length > 0 ? (
+                item.comments.map((comment) => (
+                  <div key={comment._id} className="comment-block">
+
+                    <TextField
+                      label="Comment Date"
+                      variant="outlined"
+                      disabled
+                      fullWidth
+                      margin="normal"
+                      value={comment.date_time}
+                      size="small"
+                    />
+
+                    <TextField
+                      label="User"
+                      variant="outlined"
+                      disabled
+                      fullWidth
+                      margin="normal"
+                      value={
+                        comment.user.first_name +
+                        " " +
+                        comment.user.last_name
+                      }
+                      component="a"
+                      href={"#/users/" + comment.user._id}
+                      size="small"
+                    />
+
+                    <TextField
+                      label="Comment"
+                      variant="outlined"
+                      disabled
+                      fullWidth
+                      margin="normal"
+                      multiline
+                      rows={2}
+                      value={comment.comment}
+                      size="small"
+                    />
+                  </div>
+                ))
+              ) : (
+                <TextField
+                  label="No Comments"
+                  variant="outlined"
+                  disabled
+                  fullWidth
+                  margin="normal"
+                />
+              )}
+
+              {/*  ADD COMMENT INPUT */}
+              <TextField
+                label="Add Comment"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={this.state.commentTexts[item._id] || ""}
+                onChange={(e) =>
+                  this.handleCommentChange(item._id, e.target.value)
+                }
+              />
+
+              <Button
+                variant="contained"
+                onClick={() =>
+                  this.handleSubmitComment(item._id)
+                }
+              >
+                Submit Comment
+              </Button>
+
+            </div>
+          ))}
+        </ImageList>
+
+      </div>
+    ) : (
+      <div />
+    );
   }
 }
 
